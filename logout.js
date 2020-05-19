d1fbe2b451758d2feb6c01ba91a2ca0f27ada2da
Chamy619 <38484972+Chamy619@users.noreply.github.com>
2020-05-19 17:56:27 +0900

    5th version
    로그아웃 버튼 디자인 변경

--- logout.js
+++ logout.js
@@ -1,6 +1,6 @@
 function logout() {
     if(localStorage.getItem('name') !== null) {
-        var html = '<input type="button" id="logout" value="logout" onclick="buttonClick()">';
+        var html = '<input type="button" id="logout" class="btn btn-danger" value="logout" onclick="buttonClick()">';
         document.querySelector('#logout_box').innerHTML = html;
     }
 }
